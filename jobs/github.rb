f62cbbede65b7ccdaed76226f90be2dec1bb067d
Zach Latta <zach@zachlatta.com>
2014-07-22 08:00:22 -0700

    Fix logic for getting latest commit from GitHub.

--- jobs/github.rb
+++ jobs/github.rb
@@ -5,7 +5,7 @@ GITHUB_CLIENT_SECRET = ENV['GITHUB_CLIENT_SECRET']
 
 client = Octokit::Client.new
 
-if not GITHUB_CLIENT_ID.empty? or GITHUB_CLIENT_SECRET.empty?
+if not GITHUB_CLIENT_ID.nil? or GITHUB_CLIENT_SECRET.nil?
   client = Octokit::Client.new(client_id: GITHUB_CLIENT_ID,
                                client_secret: GITHUB_CLIENT_SECRET)
 end
@@ -13,7 +13,14 @@ end
 SCHEDULER.every '5s', :first_in => 0 do |job|
   repos = client.repos('hackedu')
 
-  commits = Set.new
+  commits = Array.new
+  repos.map { |r| commits << client.commits("#{r.owner.login}/#{r.name}") }
+  commits.flatten!
 
-  commits.map { |c| puts c }
+  most_recent  = commits.max_by { |c| c.commit.author.date }.commit
+
+  short = { author: most_recent.author.name, message: most_recent.message,
+            url: most_recent.url }
+
+  send_event('most_recent_commit', { value: short })
 end
